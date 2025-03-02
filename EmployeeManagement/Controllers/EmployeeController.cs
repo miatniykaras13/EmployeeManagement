9c67eced6673ffe6ae2f36ca641117d68f3d2321
miatniykaras13 <vladosggg007@gmail.com>
2025-03-02 17:02:31 +0300

    Add project files.

---
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -0,0 +1,68 @@
+﻿using EmployeeManagement.Models;
+using EmployeeManagement.Repositories;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace EmployeeManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetAllEmployeesAsync()
+        {
+            var employees = await _employeeRepository.GetAllAsync();
+            return Ok(employees);
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<Employee>> DeleteEmployeeAsync(int id)
+        {
+            try
+            {
+                await _employeeRepository.DeleteEmployeeAsync(id);
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetEmployeeByIdAsync(int id)
+        {
+            var employee = await _employeeRepository.GetByIdAsync(id);
+
+            if(employee == null)
+                return NotFound();
+
+            return Ok(employee);
+        }
+
+        [HttpPost("{employee}")]
+        public async Task<ActionResult<Employee>> UpdateEmployeeAsync(Employee employee)
+        {
+            await _employeeRepository.UpdateEmployeeAsync(employee);
+            return Ok();
+        }
+        
+        [HttpPost]
+        public async Task<ActionResult<Employee>> CreateEmployee(Employee employee)
+        {
+            await _employeeRepository.AddEmployeeAsync(employee);
+            return CreatedAtAction(nameof(GetEmployeeByIdAsync), new {id = employee.Id}, employee);
+        }
+    }
+}
---
+++ EmployeeManagement/Repositories/EmployeeRepository.cs
@@ -0,0 +1,49 @@
+﻿using EmployeeManagement.Data;
+using EmployeeManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public EmployeeRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task AddEmployeeAsync(Employee employee)
+        {
+            await _appDbContext.Employees.AddAsync(employee);
+            await _appDbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteEmployeeAsync(int id)
+        {
+            var employee = await _appDbContext.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                throw new KeyNotFoundException($"Employee with id {id} was not found.");
+            }
+            _appDbContext.Employees.Remove(employee);
+            await _appDbContext.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Employee>> GetAllAsync()
+        {
+            return await _appDbContext.Employees.ToListAsync();
+        }
+
+        public async Task<Employee?> GetByIdAsync(int id)
+        {
+            return await _appDbContext.Employees.FindAsync(id);
+        }
+
+        public async Task UpdateEmployeeAsync(Employee employee)
+        {
+            _appDbContext.Employees.Update(employee);
+            await _appDbContext.SaveChangesAsync();
+        }
+    }
+}
